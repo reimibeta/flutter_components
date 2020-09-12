d6d23c726387579c02b9428aa926e436f02de934
reimibeta <65539218+reimibeta@users.noreply.github.com>
2020-09-12 20:49:45 +0700

    Update list_separated.dart

--- lib/src/listview/list_separated/list_separated.dart
+++ lib/src/listview/list_separated/list_separated.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
 
-// ignore: must_be_immutable
+
 class ListSeparated<T> extends StatefulWidget {
 
-  List<T> results;
-  ScrollController controller;
-  Function(T) title;
-  Function(T) subtitle;
-  Widget trailingIcon;
-  Function(T, int) onTap;
+  final ScrollController controller;
+  final List<T> results;
+  final Function itemBuilder;
 
   ListSeparated({
     Key key,
-    @required this.results,
     this.controller,
-    this.onTap,
-    @required this.title,
-    this.subtitle,
-    this.trailingIcon
+    @required this.results,
+    @required this.itemBuilder
   }) : super(key : key);
 
   @override
@@ -31,43 +25,15 @@ class _ListSeparatedState extends State<ListSeparated> {
     super.initState();
   }
 
-  Widget _title(int position){
-    return widget.title(widget.results[position]);
-  }
-
-  Widget _subtitle(int position){
-    return widget.subtitle != null ? widget.subtitle(widget.results[position]) : null;
-  }
-
-  Widget _trailing(){
-    return widget.trailingIcon != null ? Container(
-      padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 20.0),
-      child: InkWell(
-        child: widget.trailingIcon,
-      ),
-    ) : Container();
-  }
-
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
       separatorBuilder: (context, index) => Divider(
         color: Colors.grey.shade400, height: 0,
       ),
-      controller: this.widget.controller,
-      itemCount: this.widget.results.length,
-      itemBuilder: (_,int position){
-        return new ListTile(
-            title: _title(position),
-            subtitle: _subtitle(position),
-            trailing: _trailing(),
-            // ignore: unnecessary_statements
-            onTap:() async {
-              if(widget.onTap != null)
-                widget.onTap(this.widget.results[position], position);
-            }
-        );
-      },
+      controller: widget.controller,
+      itemCount: widget.results.length,
+      itemBuilder: widget.itemBuilder,
     );
   }
 }
