122598a223716bb798066b2d55ac6b583ee53c23
reimibeta <65539218+reimibeta@users.noreply.github.com>
2020-10-25 21:09:47 +0700

    Update content_date.dart

--- lib/src/content/src/content_date.dart
+++ lib/src/content/src/content_date.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
-
+import 'content_text.dart';
 
 class ContentDate extends StatelessWidget {
 
+  final String title;
   final String date;
   final String locale;
   final String error;
 
-  ContentDate({Key key, this.date, this.locale, this.error }):super(key: key);
+  ContentDate({Key key,
+    @required this.title,
+    @required this.date,
+    this.locale,
+    this.error
+  }):super(key: key);
 
   String _format(String date, {String locale}){
     DateFormat dateFormat = new DateFormat.yMMMMd(locale ?? 'km');
@@ -23,13 +29,18 @@ class ContentDate extends StatelessWidget {
     return formatted;
   }
 
-  String _date(String date, {@required String error}){
+  String _checkEmptyDate(String date, {@required String error}){
     return date == null ? error : _format(date, locale: "km");
   }
 
   @override
   Widget build(BuildContext context) {
     initializeDateFormatting();
-    return Text(_date(this.date, error: this.error ?? "not available"));
+    // return Text(_date(this.date, error: this.error ?? "not available"));
+    return ContentText(
+      title: "\t\t\t${this.title}",
+      titleStyle: TextStyle(fontWeight: FontWeight.bold),
+      content: ":\t${_checkEmptyDate(this.date, error: this.error ?? "not available")}",
+    );
   }
 }
