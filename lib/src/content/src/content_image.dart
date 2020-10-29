2ca05a71247fdf8e05709dfb6911554a71bc1a38
reimibeta <65539218+reimibeta@users.noreply.github.com>
2020-10-29 22:22:44 +0700

    fixed image

--- lib/src/content/src/content_image.dart
+++ lib/src/content/src/content_image.dart
@@ -4,7 +4,7 @@ import 'package:flutter_components/flutter_components.dart';
 class ContentImage<T> extends StatelessWidget {
 
   final List<T> models;
-  final Function thumbnail;
+  final Function(T) thumbnail;
   final Function view;
   final Text caption;
 
@@ -18,46 +18,44 @@ class ContentImage<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 300,
-        child: Stack(
-          children: <Widget>[
-            GalleyPhotoView<T>(
-              thumbnail: this.thumbnail,
-              images: this.models,
-              onTap: (index){
-                // open(context, imagePaths, index);
-                flutterGallery.open<T>(
-                    context,
-                    images: this.models,
-                    index: index,
-                    view: this.view
-                );
-              },
-              getIndex: (index){
-                print("Index: $index");
-                // blocItem.sinkImageIndex(index);
-              },
-              boxFit: BoxFit.cover,
-            ),
-            Positioned(
-              bottom: 0,
-              right: 0,
-              child: Container(
-                width: 70,
-                height: 25,
-                color: Colors.black.withOpacity(0.4),
-                child: Center(
-                  child: this.caption ?? Text(
-                    "${models.length} ${models.length > 1 ? 'pics' : 'pic'}",
-                    style: TextStyle(
-                        color: Colors.white
-                    ),
+      height: 300,
+      child: Stack(
+        children: <Widget>[
+          GalleyPhotoView<T>(
+            thumbnail: this.thumbnail,
+            images: this.models,
+            onTap: (index){
+              flutterGallery.open<T>(
+                  context,
+                  images: this.models,
+                  index: index,
+                  view: this.view
+              );
+            },
+            getIndex: (index){
+              print("Index: $index");
+            },
+            boxFit: BoxFit.cover,
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Container(
+              width: 70,
+              height: 25,
+              color: Colors.black.withOpacity(0.4),
+              child: Center(
+                child: this.caption ?? Text(
+                  "${models.length} ${models.length > 1 ? 'pics' : 'pic'}",
+                  style: TextStyle(
+                      color: Colors.white
                   ),
                 ),
               ),
-            )
-          ],
-        )
+            ),
+          )
+        ],
+      )
     );
   }
 }
